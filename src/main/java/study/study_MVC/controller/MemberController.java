15b4d6620b451a9b804d20022ea93d6f4a48c8cf
KCC <qkr2633911@naver.com>
2022-10-31 16:52:06 +0900

    회원 웹 기능 -> 조회 - 34p

--- src/main/java/study/study_MVC/controller/MemberController.java
+++ src/main/java/study/study_MVC/controller/MemberController.java
@@ -2,10 +2,13 @@ package study.study_MVC.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import study.study_MVC.domain.Member;
 import study.study_MVC.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -28,5 +31,11 @@ public class MemberController {
         member.setName(form.getName());
         memberService.join(member);
         return "redirect:/";
+    }
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
