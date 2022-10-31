a477a2a7c4336d54352b5852bc7d1d4b477d3fb3
unknown <qkr2633911@naver.com>
2022-10-31 10:39:02 +0900

    Initial commit

---
+++ src/main/java/study/study_MVC/StudyMvcApplication.java
@@ -0,0 +1,13 @@
+package study.study_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMvcApplication.class, args);
+	}
+
+}
