c21543785e8a401aa527f296d1abf4ce7a5f37e3
KCC <qkr2633911@naver.com>
2022-10-31 16:43:21 +0900

    회원 웹 기능 -> 등록 - 32p

---
+++ src/main/java/study/study_MVC/controller/MemberForm.java
@@ -0,0 +1,11 @@
+package study.study_MVC.controller;
+
+public class MemberForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
