e86f6ca5e429fe2135734118f434b58f55f5e1bf
KCC <qkr2633911@naver.com>
2022-11-08 11:28:53 +0900

    AOP 적용 - 58p

---
+++ src/main/java/study/study_MVC/aop/TimeTraceAop.java
@@ -0,0 +1,23 @@
+package study.study_MVC.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+    @Around("execution(* study.study_MVC..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
